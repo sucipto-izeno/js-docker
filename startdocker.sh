a2c2ebfe82d6a2e5cd628ab043734a0ffbca7a62
sucipto <sucipto.hardinata@izeno.com>
2019-04-05 12:19:59 +0800

    test

--- startdocker.sh
+++ startdocker.sh
@@ -6,15 +6,15 @@ JS_VERSION=7.1.1
 echo "Downloading from GCS"
 gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/jasperreports-server_${JS_VERSION}_bin.zip	 resources/
 
-gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/applicationContext-externalAuth-Keycloak.xml	 resources/WEB-INF
-gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/slave.json	 resources/WEB-INF
+gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/applicationContext-externalAuth-Keycloak.xml	 resources/WEB-INF/
+gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/slave.json	 resources/WEB-INF/
 
-gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/jasperserver-keycloak-adapter-0.0.3-SNAPSHOT.jar	 resources/WEB-INF/lib
-gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-adapter-core-2.5.5.Final.jar	 resources/WEB-INF/lib
-gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-adapter-spi-2.5.5.Final.jar	 resources/WEB-INF/lib
-gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-common-2.5.5.Final.jar	 resources/WEB-INF/lib
-gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-core-2.5.5.Final.jar	 resources/WEB-INF/lib
-gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-spring-security-adapter-2.5.5.Final.jar	 resources/WEB-INF/lib
+gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/jasperserver-keycloak-adapter-0.0.3-SNAPSHOT.jar	 resources/WEB-INF/lib/
+gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-adapter-core-2.5.5.Final.jar	 resources/WEB-INF/lib/
+gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-adapter-spi-2.5.5.Final.jar	 resources/WEB-INF/lib/
+gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-common-2.5.5.Final.jar	 resources/WEB-INF/lib/
+gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-core-2.5.5.Final.jar	 resources/WEB-INF/lib/
+gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/WEB-INF/lib/keycloak-spring-security-adapter-2.5.5.Final.jar	 resources/WEB-INF/lib/
 
 echo "Running Docker Build"
 
