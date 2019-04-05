769319badb483e85946c73bb6cbfc1cad0da1a69
sucipto <sucipto.hardinata@izeno.com>
2019-04-05 12:08:34 +0800

    updated

--- scripts/startdocker.sh
+++ scripts/startdocker.sh
@@ -3,4 +3,14 @@
 # Copyright (c) 2019. Master Sucipto
 JS_VERSION=7.1.1
 
-gsutil cp gs://gke-shared/jasperreports/${JS_VERSION}/jasperreports-server_${JS_VERSION}_bin.zip	 resources/
+gsutil cp gs://gke-shared/jasperreport/${JS_VERSION}/jasperreports-server_${JS_VERSION}_bin.zip	 resources/
+
+gsutil cp gs://gke-shared/jasperreport/WEB-INF/applicationContext-externalAuth-Keycloak.xml	 resources/WEB-INF
+gsutil cp gs://gke-shared/jasperreport/WEB-INF/slave.json	 resources/WEB-INF
+
+gsutil cp gs://gke-shared/jasperreport/WEB-INF/lib/jasperserver-keycloak-adapter-0.0.3-SNAPSHOT.jar	 resources/WEB-INF/lib
+gsutil cp gs://gke-shared/jasperreport/WEB-INF/lib/keycloak-adapter-core-2.5.5.Final.jar	 resources/WEB-INF/lib
+gsutil cp gs://gke-shared/jasperreport/WEB-INF/lib/keycloak-adapter-spi-2.5.5.Final.jar	 resources/WEB-INF/lib
+gsutil cp gs://gke-shared/jasperreport/WEB-INF/lib/keycloak-common-2.5.5.Final.jar	 resources/WEB-INF/lib
+gsutil cp gs://gke-shared/jasperreport/WEB-INF/lib/keycloak-core-2.5.5.Final.jar	 resources/WEB-INF/lib
+gsutil cp gs://gke-shared/jasperreport/WEB-INF/lib/keycloak-spring-security-adapter-2.5.5.Final.jar	 resources/WEB-INF/lib
